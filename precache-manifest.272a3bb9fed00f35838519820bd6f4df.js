3ddaf14cbc56f45d09053c06fd394df43cd59f59
Boris <ib09@list.ru>
2019-09-30 22:47:08 +0500

    Updates

---
+++ precache-manifest.272a3bb9fed00f35838519820bd6f4df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49d78ce5d03921fcc782e7183370179f",
+    "url": "/vkapps/index.html"
+  },
+  {
+    "revision": "a924933b12ad922b596c",
+    "url": "/vkapps/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "0025643544b6f587f3db",
+    "url": "/vkapps/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "a924933b12ad922b596c",
+    "url": "/vkapps/static/js/2.0b64f161.chunk.js"
+  },
+  {
+    "revision": "0025643544b6f587f3db",
+    "url": "/vkapps/static/js/main.db6cb3d4.chunk.js"
+  },
+  {
+    "revision": "f0b99226d47375b48dcd",
+    "url": "/vkapps/static/js/runtime-main.42bc7c57.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkapps/static/media/persik.4e1ec840.png"
+  }
+]);
